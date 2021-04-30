cd92682761abf6e524991672a9d8513615814483
mustapha <mustapha.elm@hotmail.fr>
2021-04-30 04:02:31 +0200

    création de la page login + ajout css

--- authentification.php
+++ authentification.php
@@ -1,8 +1,28 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire d'authentification simple</title>
+    <link rel="stylesheet" href="css/style.css" >
+</head>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<body>
+    <div id="bloc-page">
+        <header>
+            <h1>Authentification</h1>
+            <button>
+                <a href="#">S'inscrire</a>
+            </button>
+        </header>
+        <section>
+            <form>
+                <input type="text" placeholder="Login" >
+                <input type="password" placeholder="Mot de passe" >
+                <input type="submit" value="Valider" >
+            </form>
+        </section>
+    </div>
+</body>
+</html>
 
